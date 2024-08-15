88a5fdd865c75828c9c1d4644dda685b6209120f
Bsting118 <brendansting@gmail.com>
2024-08-15 13:21:30 -0400

    Updated asteroids fracture process

--- Assets/_Bsting/Scripts/Asteroids/FracturedAsteroid.cs
+++ Assets/_Bsting/Scripts/Asteroids/FracturedAsteroid.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class FracturedAsteroid : MonoBehaviour
 {
-    [SerializeField][Range(1f, 5f)] private float _duration = 2f;
+    [SerializeField][Range(1f, 60f)] private float _duration = 2f;
 
     private void OnEnable()
     {
-        Debug.Log("==== Fractured Asteroid Enabled! ====");
         Destroy(gameObject, _duration);
     }
 }
