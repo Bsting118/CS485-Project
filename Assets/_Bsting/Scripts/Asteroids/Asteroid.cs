d0a044cbf3b629c1fc2d3c5fc76ff18ab346065c
Bsting118 <brendansting@gmail.com>
2024-09-11 17:27:23 -0400

    Added lifetime to asteroids (20 seconds default)

--- Assets/_Bsting/Scripts/Asteroids/Asteroid.cs
+++ Assets/_Bsting/Scripts/Asteroids/Asteroid.cs
@@ -4,29 +4,79 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour, IDamageable
 {
+    // Public properties:
     [field: SerializeField, HideInInspector] public AsteroidUnitSpawner _parentSpawner { get; set; } = null;
     [field: SerializeField] public Vector3 TargetToHurdleTowards { get; set; }
     [field: SerializeField] public bool HasTargetBeenSet { get; set; } = false;
     [field: SerializeField] public float SpeedToApplyToHurdlingAsteroid { get; set; } = 1.0f;
+    [field: SerializeField] public float LifetimeOfAsteroid { get; set; } = 20.0f;
+
+    // Inspector-exposed fields:
     [SerializeField] private FracturedAsteroid _fracAsteroidPrefab;
     [SerializeField] private Detonator _explosionPrefab;
 
+    // Private var's:
     private Transform _transform;
+    private Rigidbody _rigidBody;
+    private Coroutine _lifeTimerRoutine = null;
+    private Vector3 _eulerAngleRotation = new Vector3(45, 0, 0); // Rotating the X axis, 45 deg/sec
     private bool _hasStartedToHurdle = false;
+    private bool _hasStartedLifetime = false;
 
+    #region Monobehaviors
     void Awake()
     {
-        _transform = transform;
+        InitAsteroidComponents();
+    }
+
+    void OnEnable()
+    {
+        InitAsteroidComponents();
+        _lifeTimerRoutine = StartCoroutine(UpdateLifetimeOfAsteroid());
+    }
+
+    void OnDisable()
+    {
+        if (_lifeTimerRoutine != null)
+        {
+            InterruptAsteroidLifetimeRoutine();
+        }
     }
 
     void FixedUpdate()
     {
-        //if (TargetToHurdleTowards != null)
-        if (HasTargetBeenSet)
+        if (HasTargetBeenSet && !_hasStartedToHurdle)
         {
             ThrowAsteroidAtTarget(TargetToHurdleTowards, atSpeed:SpeedToApplyToHurdlingAsteroid);
         }
+
+        MakeAsteroidSpin();
     }
+    #endregion
+
+    IEnumerator UpdateLifetimeOfAsteroid()
+    {
+        if (!_hasStartedLifetime)
+        {
+            float lifeTimer = LifetimeOfAsteroid;
+            _hasStartedLifetime = true;
+
+            // Wait X seconds where X is the lifetime:
+            yield return new WaitForSeconds(lifeTimer);
+
+            // Once timer is up, this is executed:
+            DespawnTheAsteroid();
+        }
+    }
+
+    #region Coroutine Helper(s)
+    public void InterruptAsteroidLifetimeRoutine()
+    {
+        StopCoroutine(_lifeTimerRoutine);
+        _lifeTimerRoutine = null;
+        _hasStartedLifetime = false;
+    }
+    #endregion
 
     #region Damage Asteroid Methods
     public void TakeDamage(int damage, Vector3 hitPosition)
@@ -49,27 +99,32 @@ public class Asteroid : MonoBehaviour, IDamageable
         }
 
         // Destroy and report dstruction of Asteroid to allow a new spawn:
-        _parentSpawner.ReportAsteroidDestroyed();
         Destroy(this.gameObject);
+        _parentSpawner.ReportAsteroidDestroyed();
+    }
+
+    private void DespawnTheAsteroid()
+    {
+        Destroy(this.gameObject);
+        // We still want to report it to get a new one:
+        _parentSpawner.ReportAsteroidDestroyed();
     }
     #endregion
 
-    // ADDED:
+    #region Throw Asteroid Methods
     private void ThrowAsteroidAtTarget(Transform target, float atSpeed = 1.0f, float throwFactorToTarget = 2f)
     {
         // (this.gameObject = asteroid object)
-
-        // Assuming that asteroid object has a RigidBody for applied physics:
-        Rigidbody asteroidRB = this.gameObject.GetComponent<Rigidbody>();
+        // (_rigidBody = this asteroid's RB)
 
         /* --- Setting up Vector line to find the endpoint where the Asteroid should be moved/shot to --- */
         // Intersect and extend past the target
         Vector3 headingOfAsteroid = (target.position - _transform.position) * throwFactorToTarget;
 
-        // Using AddForce() instead of  MovePosition() now:
+        // Using AddForce() instead of MovePosition() now:
         if (!_hasStartedToHurdle)
         {
-            asteroidRB.AddForce(headingOfAsteroid * atSpeed);
+            _rigidBody.AddForce(headingOfAsteroid * atSpeed);
             _hasStartedToHurdle = true;
         }
 
@@ -78,20 +133,45 @@ public class Asteroid : MonoBehaviour, IDamageable
     private void ThrowAsteroidAtTarget(Vector3 target, float atSpeed = 1.0f, float throwFactorToTarget = 2f)
     {
         // (this.gameObject = asteroid object)
-
-        // Assuming that asteroid object has a RigidBody for applied physics:
-        Rigidbody asteroidRB = this.gameObject.GetComponent<Rigidbody>();
+        // (_rigidBody = this asteroid's RB)
 
         /* --- Setting up Vector line to find the endpoint where the Asteroid should be moved/shot to --- */
         // Intersect and extend past the target
         Vector3 headingOfAsteroid = (target - _transform.position) * throwFactorToTarget;
 
-        // Using AddForce() instead of  MovePosition() now:
+        // Using AddForce() instead of MovePosition() now:
         if (!_hasStartedToHurdle)
         {
-            asteroidRB.AddForce(headingOfAsteroid * atSpeed);
+            _rigidBody.AddForce(headingOfAsteroid * atSpeed);
             _hasStartedToHurdle = true;
         }
 
     }
+    #endregion
+
+    #region Rotate Asteroid Method(s)
+    private void MakeAsteroidSpin()
+    {
+        if (_hasStartedToHurdle && _rigidBody != null)
+        {
+            Quaternion deltaRotation = Quaternion.Euler(_eulerAngleRotation * Time.fixedDeltaTime);
+            _rigidBody.MoveRotation(_rigidBody.rotation * deltaRotation);
+        }
+    }
+    #endregion
+
+    #region Helper Method(s)
+    private void InitAsteroidComponents()
+    {
+        if (_transform == null || _transform != transform)
+        {
+            _transform = transform;
+        }
+
+        if (_rigidBody == null)
+        {
+            _rigidBody = this.gameObject.GetComponent<Rigidbody>();
+        }
+    }
+    #endregion
 }
